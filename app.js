47e41a4f0327d8ddf084a42a5ff815e0423f56bc
mkhaque1 <mkhaque29@gmail.com>
2026-06-01 22:30:53 +0200

    finalize project

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const searchInput = document.getElementById('searchInput');
 
 // task array
 
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 // add task button event listener
 
@@ -15,7 +15,7 @@ addBtn.addEventListener('click', addTask);
 // addTask function
 
 function addTask() {
-  const title = taskInput.value;
+  const title = taskInput.value.trim();
 
   // condition
   if (title === '') {
@@ -36,20 +36,23 @@ function addTask() {
   };
   tasks.push(newTask);
 
+  taskInput.value = '';
+
   renderTask();
 }
 
 // render in UI
 
 function renderTask(taskArray = tasks) {
-  taskArray.forEach((task) => {
-    taskList.innerHTML = '';
-    // dynamic HTML
+  taskList.innerHTML = '';
 
+  // loop for array items
+  taskArray.forEach((task) => {
+    // dynamic HTML
     const div = document.createElement('div');
 
     div.className =
-      'flex justify-between items-center bg-gray-100 p-4 rounded-xl';
+      'flex mb-2 justify-between items-center bg-gray-100 p-4 rounded-xl';
 
     div.innerHTML = `<div>
         <h3 class='${task.status === 'completed' ? 'line-through text-green-600' : 'text-purple-600'}'>${task.title}</h3> 
@@ -60,13 +63,13 @@ function renderTask(taskArray = tasks) {
         <div>
         <button
           onclick="toggleStatus(${task.id})"
-          class="bg-green-500 text-white px-3 py-1 rounded"
+          class="bg-green-500 text-white px-3 py-1 rounded cursor-pointer"
         >
           Done
         </button>
         <button
           onclick="deleteTask(${task.id})"
-          class="bg-red-500 text-white px-3 py-1 rounded"
+          class="bg-red-500 text-white px-3 py-1 rounded cursor-pointer"
         >
           Delete
         </button>
@@ -74,34 +77,32 @@ function renderTask(taskArray = tasks) {
         
         `;
 
-    count.textContent = tasks.length;
-
     taskList.appendChild(div);
 
     console.log(div);
   });
+
+  count.textContent = tasks.length;
+
+  saveTasks();
 }
 
 // toogleStatus
 
 function toggleStatus(id) {
-  const task = tasks.find((task) => {
-    task.id === id;
+  const task = tasks.find((task) => task.id === id);
 
-    if (task) {
-      task.status = task.status === 'pending' ? 'completed' : 'pending';
+  if (task) {
+    task.status = task.status === 'pending' ? 'completed' : 'pending';
 
-      renderTask();
-    }
-  });
+    renderTask();
+  }
 }
 
 // delete items
 
 function deleteTask(id) {
-  tasks = tasks.filter((task) => {
-    task.id !== id;
-  });
+  tasks = tasks.filter((task) => task.id !== id);
 
   console.log(tasks);
 
@@ -113,8 +114,17 @@ function deleteTask(id) {
 searchInput.addEventListener('input', () => {
   const value = searchInput.value.toLowerCase();
 
-  const filteredTask = tasks.filter((task) => {
-    task.title.toLowerCase().includes(value);
-  });
-  console.log(filteredTask);
+  const filteredTasks = tasks.filter((task) =>
+    task.title.toLowerCase().includes(value),
+  );
+
+  renderTask(filteredTasks);
 });
+
+// local storage save function
+
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+renderTask();
